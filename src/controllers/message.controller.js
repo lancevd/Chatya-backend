f9f6c85f9a4061b4592fbe8a69c435b7c76a4156
lancevd <olamosob@gmail.com>
2025-02-05 18:31:12 +0100

    get all users

---
+++ src/controllers/message.controller.js
@@ -0,0 +1,14 @@
+import User from "../models/user.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUser = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUser },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.js";
+import messageRoutes from "./routes/message.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port:" + PORT);
