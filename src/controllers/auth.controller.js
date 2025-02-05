dd5c908a9acad780e63ed52b08b95f93968e3c1d
lancevd <olamosob@gmail.com>
2025-02-05 17:54:06 +0100

    Check

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -112,7 +112,7 @@ export const logout = (req, res) => {
 
 export const updateProfile = async (req, res) => {
   try {
-    const {profilePic} = req.body;
+    const { profilePic } = req.body;
     const userId = req.user._id;
 
     if (!profilePic) {
@@ -130,7 +130,18 @@ export const updateProfile = async (req, res) => {
       ...updatedUser._doc,
       password: undefined,
     });
+  } catch (error) {}
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json({
+      user: req.user,
+    });
   } catch (error) {
-    
+    console.log("Check auth controller error", error.message);
+    res.status(500).json({
+      message: `Internal server error: ${error}`,
+    });
   }
-}
+};
