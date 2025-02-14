59d716022b86c654587e7eff03be18c9fec51fc2
lancevd <olamosob@gmail.com>
2025-02-14 11:05:28 +0100

    user default pic

--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,30 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        fullName: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true,
-            minlength: 5
-        },
-        profilePic: {
-            type: String,
-            default: ""
-        }
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {timestamps: true}
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/ddydgzuu7/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_white,b_rgb:262c35/v1721993897/samples/smile.jpg",
+    },
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
