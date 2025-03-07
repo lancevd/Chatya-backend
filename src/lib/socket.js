cbe3474a61856bd50e600b8c746f788e74b59ae0
lancevd <olamosob@gmail.com>
2025-03-07 20:42:10 +0100

    .

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,23 +1,23 @@
-import {server} from 'socket.io';
-import http from 'http';
-import express from 'express';
-
+import { server } from "socket.io";
+import http from "http";
+import express from "express";
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-    },
-})
+  cors: {
+    origin: "*",
+  },
+});
 
-io.on('connection', (socket) => {
-    console.log("A user connected", socket.id);
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
 
-    socket.on('disconnect', () => {
-        console.log("A user disconnected", socket.id);
-    })
-})
+  // listen for a disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+  });
+});
 
-export {io, app, server};
+export { io, app, server };
