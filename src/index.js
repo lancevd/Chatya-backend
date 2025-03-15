96aa6dcb295a59128f4b8a5138b28dab067d53b5
lancevd <olamosob@gmail.com>
2025-03-15 15:41:25 +0100

    cors prod

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chatya-backend.vercel.app" || "http://localhost:3000",
+    origin: "https://chatya-backend.vercel.app",
     credentials: true,
   })
 );
