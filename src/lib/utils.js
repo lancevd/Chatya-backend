adc0a6e341e2057867c16a59fca39d75f94714c9
lancevd <olamosob@gmail.com>
2025-05-16 10:32:44 +0100

    Samesite fixed

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,6 +9,6 @@ export const generateToken = (userId, res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
   });
 }
